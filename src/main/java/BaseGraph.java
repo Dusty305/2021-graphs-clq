807b3e5ae6ef00e9b6af2c2f0ad426cfe8d02406
Dusty305 <lacid305123@bk.ru>
2021-10-25 23:49:48 +0300

    not working BaseGraph

---
+++ src/main/java/BaseGraph.java
@@ -0,0 +1,46 @@
+import com.mathsystem.entity.graph.Color;
+import com.mathsystem.graphapi.AbstractEdge;
+import com.mathsystem.graphapi.AbstractGraph;
+import com.mathsystem.graphapi.Vertex;
+import com.mathsystem.plugin.GraphProperty;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class BaseGraph implements GraphProperty {
+    private HashSet<Vertex> reachedVertecies;
+
+    @Override
+    public boolean execute(AbstractGraph abstractGraph) {
+        ArrayList<Vertex> baseVertecies = new ArrayList<>();
+        reachedVertecies = new HashSet<>();
+
+        // Все красные вершины - проверяемая база графа
+        for (Vertex v : abstractGraph.getVertices()) {
+            if (v.getColor() == Color.red) {
+                System.out.println("Reachable vertex: " + v.getIndex());
+                baseVertecies.add(v);
+                reachedVertecies.add(v);
+            }
+        }
+
+        for(Vertex bv : baseVertecies){
+            reachEach(bv);
+        }
+
+        if(abstractGraph.getVertexCount() == reachedVertecies.size())
+            return true;
+        else
+            return false;
+    }
+
+    void reachEach(Vertex vertex){
+        for (AbstractEdge e : vertex.getEdgeList()){
+            Vertex reachedVertex = e.getW();
+            if (!reachedVertecies.contains(reachedVertex)) {
+                reachedVertecies.add(reachedVertex);
+                reachEach(reachedVertex);
+            }
+        }
+    }
+}
--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,13 +5,10 @@ import java.io.File;
 
 public class MainClass {
     public static void main(String[] args) {
-        // Для теста написанного модуля, напишите следующий код
-        // Вместо ClassName и className написать название класса с написанным модулем
-        /*
-        ClassName className = new ClassName();
+        BaseGraph graphSquare = new BaseGraph();
+
         AbstractGraph abstractGraph = GraphFactory.loadDirectedGraphFromFile(
-               new File("ПУТЬ ДО ПАПКИ С ГРАФОМ", "НАЗВАНИЕ ФАЙЛА С ГРАФОМ"));
-        System.out.println(className.execute(abstractGraph));
-        */
+                new File("C://Users//user//Downloads", "graph (2).txt"));
+        System.out.println(graphSquare.execute(abstractGraph));
     }
 }
