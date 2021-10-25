3e75f7b4877aa82e0ce52738a85702c695c5e434
Dusty <lacid305123@bk.ru>
2021-10-25 23:35:57 +0300

    MainClass added (for testing)

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+import com.mathsystem.graphapi.AbstractGraph;
+import com.mathsystem.graphapi.GraphFactory;
+
+import java.io.File;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Для теста написанного модуля, напишите следующий код
+        // Вместо ClassName и className написать название класса с написанным модулем
+        /*
+        ClassName className = new ClassName();
+        AbstractGraph abstractGraph = GraphFactory.loadDirectedGraphFromFile(
+               new File("ПУТЬ ДО ПАПКИ С ГРАФОМ", "НАЗВАНИЕ ФАЙЛА С ГРАФОМ"));
+        System.out.println(className.execute(abstractGraph));
+        */
+    }
+}
